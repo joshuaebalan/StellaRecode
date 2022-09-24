a0937e127c89e4c51e1bd8d044c47dfd4d58e2d5
joshuaebalan <jbalan@purdue.edu>
2022-09-24 13:43:27 -0400

    Beginning Java Version

---
+++ C Version/Resevoir.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+/*
+ * Creates a RESEVOIR Object.
+ * See README.md for details
+ */
+public class Resevoir {
+    public  Resevoir(String name) {
+        this.name = name;
+        VALUE = 0;
+        ArrayList<Flow> Flows_In = new ArrayList<>();
+        Arraylist<Flow> Flows_Out = new ArrayList<>();
+        net_flow = null;
+    }
+    public Resevoir(String name, float VALUE) {
+        this.name = name;
+        this.VALUE = VALUE;
+        ArrayList<Flow> Flows_In = new ArrayList<>();
+        Arraylist<Flow> Flows_Out = new ArrayList<>();
+        net_flow = null;
+    }
+    public float getVALUE() {
+        return VALUE;
+    }
+    public void setVALUE(float VALUE) {
+        this.VALUE = VALUE;
+    }
+
+    public ArrayList<Flow> getFlows_In() {
+        return Flows_In;
+    }
+
+    public Arraylist<Flow> getFlows_Out() {
+        return Flows_Out;
+    }
+
+    public Equation getNet_flow() {
+        return net_flow;
+    }
+
+    public void setFlows_In(ArrayList<Flow> Flows_In) {
+        this.Flows_In = Flows_In;
+    }
+
+    public void setFlows_Out(Arraylist<Flow> Flows_Out) {
+        this.Flows_Out = Flows_Out;
+    }
+
+    public void setNet_flow(Equation Net_Flow) {
+        this.Net_Flow = Net_Flow;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private float VALUE;
+    private ArrayList<Flow> Flows_In;
+    private Arraylist<Flow> Flows_Out;
+    private Equation Net_Flow;
+    private String name;
+}
